f2450d82fa6758be935125d15a71c6c941fc1d28
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-04-30 14:24:19 +0800

    make it better

--- rj3y/ticket/views.py
+++ rj3y/ticket/views.py
@@ -6,70 +6,100 @@ import pandas
 
 def index(request):
 
-    context = {
-        'error_message': 'Sorry, something may go wrong.',
-        'full_html': None,
-    }
     if not request.POST:
-        return render(request, 'index.html', context)
+        return render(request, 'ticket/index.html')
     else:
+        context = {
+            'error_message': 'Sorry, somthing is wrong. ',
+            'result_html': None,
+        }
         try:
+            # initial input
             account = request.POST.get('account')
             password = request.POST.get('password')
             session = requests.session()
-            session = get_session_login(account=account, password=password, session=session)
-            df_handling_ticket = get_df_handling_ticket(session)
-            df_handling_ticket_with_title = let_the_first_row_be_column_title(df_handling_ticket)
-            handling_ticket_number_list = list(df_handling_ticket_with_title['單號'])
+            chracter_choose = 'cloud' # could choose "cloud" or "yourself"
+            ticket_type = 'handling'
+            search_column = 'disp_seq'
+            query_string = ''
+            max_count = None
+            chracters = {
+                'cloud': '20',
+                'yourself': account,
+            }
+            context['error_message'] += 'Initial input is done.'
+
+            login_with_given_session(account=account, password=password, session=session)
+            search_http_response = search_on_dashboard_terminal(
+                session=session,
+                character=chracters[chracter_choose],
+                search_column=search_column,
+                query_string=query_string,
+                max_count=max_count
+            )
+            ticket_tables = get_dataframe_list_by_reading_html(search_http_response.text)
+            # take the part we want as a dictionary
+            ticket_tables = {
+                'preordered': ticket_tables[2],
+                'assigned': ticket_tables[3],
+                'handling': ticket_tables[4],
+                'finished': ticket_tables[5],
+                'special': ticket_tables[6],
+            }
+            for key, value in ticket_tables.items():
+                ticket_tables[key] = let_the_first_row_be_column_title(value)
             df_list = []
-            for ticket_number in handling_ticket_number_list:
-                df_ticket_detail = get_df_ticket_detail(session=session, account=account, ticket_number=ticket_number)
-                df_ticket_detail_with_title = let_the_first_row_be_column_title(df_ticket_detail)
-                df_ticket_detail_with_title_with_ticket_number = df_ticket_detail_with_title.assign(單號=ticket_number)
-                df_list += [df_ticket_detail_with_title_with_ticket_number]
-            df_ticket_detail_total = pandas.concat(df_list)
-            df_left = df_ticket_detail_total
-            df_right = df_handling_ticket_with_title.drop(columns=['流程'])
-            result = pandas.merge(df_left, df_right, on='單號', how='left')
-            context['full_html'] = result.to_html()
+            for ticket_number in list(ticket_tables[ticket_type]['單號']):
+                ticket_detail_response = get_ticket_detail_with_given_ticket_number(session=session, chracter=chracters[chracter_choose], ticket_number=ticket_number)
+                df_ticket_detail = get_dataframe_list_by_reading_html(ticket_detail_response.text)[0]
+                df_ticket_detail = let_the_first_row_be_column_title(df_ticket_detail)
+                df_ticket_detail = df_ticket_detail.assign(單號=ticket_number)
+                df_list += [df_ticket_detail]
+            df_left = pandas.concat(df_list)
+            df_right = ticket_tables[ticket_type]
+            db_joined = pandas.merge(df_left, df_right, on='單號', how='outer', suffixes=('', '-細項'))
+            context['result_html'] = db_joined.to_html()
         except:
             pass
-        return render(request, 'summary.html', context)
+        return render(request, 'ticket/summary.html', context)
 
-def get_session_login(account, password, session):
-    login_url = 'http://202.3.168.17:8080/login_check.jsp'
-    login_data = {
+def login_with_given_session(account, password, session):
+    post_url = 'http://202.3.168.17:8080/login_check.jsp'
+    post_data = {
         'sess_id': account,
         'sess_password': password,
-        'dfURL': '',
-        'dfFrame_no': 2,
-        'dfAction': '',
-        'dfId': '',
-        'dfForm': 'login.jsp'
+        'dfForm': 'login.jsp',
     }
-    session.post(url=login_url, data=login_data)
-    return session
+    http_response = session.post(url=post_url, data=post_data)
+    return http_response
 
-def get_df_handling_ticket(session):
-    dashboard_terminal_url = 'http://202.3.168.17:8080/Disp/DashBoard_Terminal.jsp'
-    response_dashboard_terminal = session.get(url=dashboard_terminal_url)
-    df_read_html_response_dashboard_terminal_text = pandas.read_html(response_dashboard_terminal.text)
-    df_handling_ticket = df_read_html_response_dashboard_terminal_text[4]
-    return df_handling_ticket
+def search_on_dashboard_terminal(session, character, search_column, query_string, max_count):
+    post_url = 'http://202.3.168.17:8080/Disp/DashBoard_Terminal.jsp?action=search'
+    post_data = {
+        'recordCount': max_count,
+        'searchColumn': search_column,
+        'searchCondition': query_string,
+        'doSearch': 'Search',
+        'character': character,
+    }
+    http_response = session.post(url=post_url, data=post_data)
+    return http_response
+
+def get_dataframe_list_by_reading_html(html):
+    return pandas.read_html(html)
 
 def let_the_first_row_be_column_title(dataframe):
     new_dataframe = dataframe[1:]
     new_dataframe.columns = dataframe.iloc[0]
     return new_dataframe
 
-def get_df_ticket_detail(session, account, ticket_number):
-    ticket_detail_url = 'http://202.3.168.17:8080/Disp/retriveDetail.jsp'
-    ticket_detail_data = {
+def get_ticket_detail_with_given_ticket_number(session, chracter, ticket_number):
+    post_url = 'http://202.3.168.17:8080/Disp/retriveDetail.jsp'
+    post_data = {
         'method': 'get_Disp_DetailCons',
-        'Disp_Cons_Seq': account,
+        'Disp_Cons_Seq': chracter,
+        'Disp_Grp_Seq': chracter,
         'Disp_Seq': ticket_number,
     }
-    response_ticket_detail = session.post(url=ticket_detail_url, data=ticket_detail_data)
-    df_read_html_response_ticket_detail_text = pandas.read_html(response_ticket_detail.text)
-    df_ticket_detail = df_read_html_response_ticket_detail_text[0]
-    return df_ticket_detail
+    http_response = session.post(url=post_url, data=post_data)
+    return http_response
