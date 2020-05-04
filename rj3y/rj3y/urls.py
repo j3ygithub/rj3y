77621e2278c3d0b1e38098a4bdbf79a6059fa23a
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-05-04 10:03:24 +0800

    update static thing

--- rj3y/rj3y/urls.py
+++ rj3y/rj3y/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('ticket/', include('ticket.urls')),
     path('', RedirectView.as_view(url='ticket/')),
 ]
+# my urls
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
